d69a04ed3e76431bfce64c8102e183e0ff0d68db
KirbyPatrick <pkirbyinatx@gmail.com>
2019-05-11 10:28:12 -0500

    added state to CreateTodo.js component

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const CreateTodo = () => {
-  return (
-    <div>
-      <p>Welcome to Create Todo Component!</p>
-    </div>
-  );
-};
+class CreateTodo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    };
+  }
+
+  onChangeTodoDescription(e) {
+    this.setState({
+      todo_description: e.target.value
+    });
+  }
+
+  onChangeTodoResponsible(e) {
+    this.setState({
+      todo_responsible: e.target.value
+    });
+  }
+
+  onChangeTodoPriority(e) {
+    this.setState({
+      todo_priority: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    
+
+    this.setState({
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    });
+  }
+
+  render() {
+    return <div />;
+  }
+}
 
 export default CreateTodo;
