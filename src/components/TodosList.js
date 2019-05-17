49b374cbe6f591e911f853ae5bb8f7c4cb9da354
KirbyPatrick <pkirbyinatx@gmail.com>
2019-05-16 23:05:19 -0500

    Started working on getting the back and front ends connected. Currently working on TodosList.js.

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const TodosList = () => {
-  return (
-    <div>
-      <p>Welcome to Todos List Component!</p>
-    </div>
-  );
-};
+export default class TodosList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [] };
+  }
 
-export default TodosList;
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/todos/")
+      .then(response => {
+        this.setState({ todos: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <p>Welcome to Todos List Component!</p>
+      </div>
+    );
+  }
+}
