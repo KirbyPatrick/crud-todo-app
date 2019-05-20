25d4870f0329e493e77f64356af313d2b3e286aa
KirbyPatrick <pkirbyinatx@gmail.com>
2019-05-20 17:42:40 -0500

    added table and styling to front end

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+
+const Todo = props => (
+  <tr>
+    <td>{props.todo.todo_description}</td>
+    <td>{props.todo.todo_responsible}</td>
+    <td>{props.todo.todo_priority}</td>
+    <Link to={"/edit/" + props.todo._id}>Edit</Link>
+  </tr>
+);
 
 export default class TodosList extends Component {
   constructor(props) {
@@ -18,10 +27,28 @@ export default class TodosList extends Component {
         console.log(error);
       });
   }
+
+  TodoList() {
+    return this.state.todos.map(function(currentTodo, i) {
+      return <Todo todo={currentTodo} key={i} />;
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>Welcome to Todos List Component!</p>
+        <h3 style={{ marginTop: 10 }}>Todo List</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Responsible</th>
+              <th>Priority</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.TodoList()}</tbody>
+        </table>
       </div>
     );
   }
